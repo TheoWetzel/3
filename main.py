78f459d28b31a1ae969800469351182767c47b82
Théo Wetzel <theowetzel7+github@gmail.com>
2024-05-11 21:26:13 +0200

    je suis toujour la

--- main.py
+++ main.py
@@ -15,32 +15,25 @@ print("3. deconnecter")
 
 nbr = int(input("que souhaiter vous faire : "))
 
-user_list = ["theo",]
-mdp_list = ["ok",]
+user_list = []
+mdp_list = []
 
-if nbr == 1:
+def connection():
     Username_verification = input("Mettez votre nom d'utilisateur : ")
     Password_verification = input("Mettez votre mots de passe : ")
-    if Username_verification != user_list:
-        print("votre nom d'utilisateur ou votre mots de passe est incorrecte")
-    elif Password_verification != mdp_list:
-        print("votre nom d'utilisateur ou votre mots de passe est incorrecte")
-    else:
-        print("vous etes connecter")
-elif nbr == 2:
+def inscription():
     Username_inscription = str(input("mettez votre nom d'utilisateur : "))
     Password_inscription = input("mettez votre mots de passe : ")
-    Password_inscription2 = input("re mettez votre mots de passe : ")
-    if Password_inscription : # mettre une retestriction pour eviter qu'il y a rien
-        pass
-    elif Username_inscription : # mettre une restriction pour eviter qu'il y a rien
-        pass
-    elif Password_inscription != Password_inscription2:
-        print("Les mots de passe ne correspondent pas")
+    Password_inscription2 = input("re-mettez votre mots de passe : ")
+
+    if Password_inscription != Password_inscription2:
+        print("Le mots de passe ne correspondent pas")
     else:
-        print("Les mots de passe correspondent bien")
-        user_list.append(Username_inscription)
-        mdp_list.append(Password_inscription2)
+        print("Le mots de passe correspondent bien")
+
+if nbr == 1:
+    connection()
+elif nbr == 2:
+    inscription()
 elif nbr == 3:
-    print("vous vous deconnecter de nos services")
-    exit
+    print("vous êtes déconnecter")
