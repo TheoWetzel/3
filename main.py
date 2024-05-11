9b8cd076b066be71693e1f4602c2b74431c5386a
Théo Wetzel <theowetzel7+github@gmail.com>
2024-05-11 19:39:58 +0200

    toujour moi

--- main.py
+++ main.py
@@ -19,9 +19,11 @@ user_list = []
 mdp_list = []
 
 if nbr == 1:
-    Username_verification = str(input("Mettez votre nom d'utilisateur"))
-    Password_verification = input("Mettez votre mots de passe")
-    if Username_verification != user_list + Password_verification != mdp_list:
+    Username_verification = input("Mettez votre nom d'utilisateur : ")
+    Password_verification = input("Mettez votre mots de passe : ")
+    if Username_verification != user_list:
+        print("votre nom d'utilisateur ou votre mots de passe est incorrecte")
+    elif Password_verification != mdp_list:
         print("votre nom d'utilisateur ou votre mots de passe est incorrecte")
     else:
         print("vous etes connecter")
@@ -29,12 +31,15 @@ elif nbr == 2:
     Username_inscription = str(input("mettez votre nom d'utilisateur : "))
     Password_inscription = input("mettez votre mots de passe : ")
     Password_inscription2 = input("re mettez votre mots de passe : ")
-    if Password_inscription != Password_inscription2:
+    if Password_inscription : # mettre une retestriction pour eviter qu'il y a rien
+        pass
+    elif Username_inscription : # mettre une restriction pour eviter qu'il y a rien
+        pass
+    elif Password_inscription != Password_inscription2:
         print("Les mots de passe ne correspondent pas")
-        user_list.append(Username_inscription)
-        mdp_list.append(Password_inscription2)
     else:
         print("Les mots de passe correspondent bien")
+        user_list.append(Username_inscription)
         mdp_list.append(Password_inscription2)
 elif nbr == 3:
     print("vous vous deconnecter de nos services")
